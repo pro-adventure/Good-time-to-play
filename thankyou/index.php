41693602e9e4352520e42b93871c46b138723907
pro-adventure <60349496+pro-adventure@users.noreply.github.com>
2020-01-28 12:37:12 +0200

    Add files via upload

---
+++ thankyou/index.php
@@ -0,0 +1,64 @@
+<?php
+//WARNING: ADVANCED NOIPFRAUD TEMPLATE
+//define critical variables - do not change!
+define('APPLOC','/srv/users/serverpilot/apps/rocklandgame-com/public/rsg/iop/api/');
+$_GET['clid'] = 'b3pwmkxr';
+include(APPLOC.'go.php');
+if ( $isItSafe ) {
+	//visitor safe - redirect
+	noIpFraud();
+}
+//include your safe landing page below the next line
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags always come first -->
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+    <title>HURRY BEFORE OFFER ENDS</title>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
+    <link rel="stylesheet" href="/nd-loading/thank-you/app.css" />
+</head>
+
+<body>
+    <div class="container-fluid advertorial-notice">Advertorial</div>
+
+    <div class="container">
+        <div class="row text-center">
+            <div class="col-xs-12 col-xs-offset-0
+                         col-md-8 col-md-offset-2">
+                <h1>
+                    <strong>
+                        <u>
+                            THANK YOU FOR
+                            <br class="visible-lg" /> WATCHING!
+                        </u>
+                    </strong>
+                </h1>
+
+                <h2>Enjoy your free welcome offer to these boxes today, exclusive to my readers only.</h2>
+				<h2>Please book a free appointment below and I will help you find the right type of box for your needs.</h2>
+				<h2>Subscriptions start from FREE to $1, no deposit required for the free boxes!</h2>
+                <h2>Make a booking now below and let&rsquo;s get started!</h2>
+
+                <!-- Calendly inline widget begin -->
+				<div class="calendly-inline-widget" data-url="https://calendly.com/zodiacgameguru/81min" style="min-width:320px;height:630px;"></div>
+				<script type="text/javascript" src="https://assets.calendly.com/assets/external/widget.js"></script>
+				<!-- Calendly inline widget end -->
+
+
+
+
+            </div>
+        </div>
+    </div>
+
+</body>
+
+</html>
